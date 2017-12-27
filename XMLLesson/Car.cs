f008418e2fbf1d9bc2bb9667f4d881fcfbceaf29
UkiOnna <sikirovtemirlan@mail.ru>
2017-12-27 17:49:24 +0600

    df

---
+++ XMLLesson/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLLesson
+{
+   public class Car
+    {
+        public Guid Id { get; set; }
+        public string Mark { get; set; }
+        public DateTime MadeDate { get; set; }
+        public int Rating { get; set; }
+        
+    }
+}
---
+++ XMLLesson/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XMLLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(@"Food.xml");
+            var rootElement = document.LastChild;
+
+            Console.WriteLine(rootElement.Name);
+
+            foreach (XmlElement element in rootElement)
+            {
+                Console.WriteLine("  " + element.LocalName);
+
+               foreach(XmlElement el in element)
+                {
+                    Console.WriteLine("    "+el.LocalName+"  ");
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
